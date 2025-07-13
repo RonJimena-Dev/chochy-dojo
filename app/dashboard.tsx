8835832492531d42f345674ed7420c67a349f107
Ronald Jimena <83937001+RonJimena-Dev@users.noreply.github.com>
2025-07-13 15:30:59 +0800

    initial commit: vite + tailwind + react

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -129,6 +129,7 @@ export default function Dashboard() {
             <option value="Patient">Patient</option>
           </select>
           <button onClick={submitTrade} className="bg-blue-600 text-white px-4 py-2 rounded-xl">
+            Submit Trade
           </button>
           </div>
       )
