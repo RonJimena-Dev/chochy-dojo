893d301db1b2a9173b2a5a70993a5ae335f22260
Ronald Jimena <83937001+RonJimena-Dev@users.noreply.github.com>
2025-07-13 13:12:58 +0800

    initial commit: vite + tailwind + react

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-3xl font-bold text-green-400">Welcome to Chochy Dojo 🐕🔥</h1>
+    </div>
+  )
+}
