25d17a18d784f324b0984ec477dc8934e444ee9b
Ronald Jimena <83937001+RonJimena-Dev@users.noreply.github.com>
2025-07-13 15:15:13 +0800

    initial commit: vite + tailwind + react

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Page from '../app/page'
+import Page from '../app/page.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
